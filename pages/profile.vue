4d96340ed563ecc650d2fa02a7cc442cda2c90eb
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-08-14 16:10:17 +0900

    プロフィール画面用のページ

---
+++ pages/profile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="profile">
+    <p>this is profile</p>
+    <p>id : {{ id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    const id = this.$route.query.id;
+    this.id = id;
+  },
+  data() {
+    return {
+      id: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
