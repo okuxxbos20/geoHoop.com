e19c99ae3a083953060c04d7778a94377c93ae92
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-08-15 20:53:19 +0900

    footerをコンポーネント化

---
+++ components/FooterDefault.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer>
+    <div class="items">
+      <p>about</p>
+      <p>legal</p>
+      <p>help</p>
+    </div>
+    <p class="copylight">
+      <small lang="en">©2020 geoHoop.com</small>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  width: 70%;
+  text-align: center;
+  margin: 0 auto;
+  .items {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding-top: 8px;
+    margin: 0 auto 8px;
+    border-top: 1px solid #ddd;
+    p {
+      color: #888;
+      font-size: 15px;
+      margin: 0 10px;
+      transition: 200ms;
+      &:hover {
+        color: #111;
+        cursor: pointer;
+      }
+    }
+  }
+  .copylight {
+    color: #888;
+    margin: 5px 0 10px;
+  }
+}
+</style>
