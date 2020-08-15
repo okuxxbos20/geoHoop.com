83ea910b97ed5b1bbb2849c809e6823cffa84ff4
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-08-15 17:00:28 +0900

    検索結果をflexbox化

--- pages/index.vue
+++ pages/index.vue
@@ -8,28 +8,22 @@
       </div>
       <div class="search-result">
         <div
-          v-for="(arr, idx) in list"
+          v-for="(court, idx) in courts"
           :key="idx"
-          class="arrRow"
+          class="newarr"
+          @click="moveTo(court.id)"
         >
-          <div
-            v-for="(court, idx) in arr"
-            :key="idx"
-            class="card"
-            @click="moveTo(court.id)"
-          >
-            <div class="overlay">
-              <div class="info likes">
-                <HeartIcon />
-                <p>{{ court.likes }}</p>
-              </div>
-              <div class="info bookmarks">
-                <BookmarkIcon />
-                <p>{{ court.bookmarks }}</p>
-              </div>
+          <div class="overlay">
+            <div class="info likes">
+              <HeartIcon />
+              <p>{{ court.likes }}</p>
             </div>
-            <img :src="court.img[0]">
+            <div class="info bookmarks">
+              <BookmarkIcon />
+              <p>{{ court.bookmarks }}</p>
+            </div>
           </div>
+          <img :src="court.img[0]">
         </div>
       </div>
     </main>
@@ -46,31 +40,19 @@ export default {
   components: { HeartIcon, BookmarkIcon },
   data() {
     return {
-      results: [],
-      list: []
+      courts: []
     }
   },
-  mounted() {
+  created() {
     db.collection('court').get().then((court) => {
       court.forEach((v) => {
-        this.results.push(v.data());
+        this.courts.push(v.data());
       });
-      this.clacArray();
     }).catch((error) => {
       console.log(error);
     });
   },
   methods: {
-    clacArray() {
-      const len = this.results.length;
-      const need = Math.floor((len + 2) / 3);
-      this.list = Array.from({ length: need }, () => []);
-      this.results.map((v, idx) => {
-        const quo = Math.floor(idx / 3);
-        const mod = idx % 3;
-        this.list[quo][mod] = v;
-      });
-    },
     moveTo(id) {
       this.$router.push({ name: 'court', query: { id: id } });
     }
@@ -98,51 +80,44 @@ main {
   .search-result {
     width: 100%;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    flex-wrap: wrap;
     border-top: 1px solid #eee;
-    .arrRow {
-      width: 100%;
-      display: flex;
-      flex-direction: row;
-      border: none;
-      .card {
-        width: calc(100% / 3);
-        height: auto;
-        // ここをwidthと同じ大きさにしたい
-        border: none;
-        position: relative;
-        .overlay {
-          width: 100%;
-          height: 100%;
-          background: rgba(#000, 0.55);
-          opacity: 0;
-          position: absolute;
-          top: 0;
-          left: 0;
-          transition: 200ms;
+    .newarr {
+      width: calc(100% / 3);
+      height: auto;
+      position: relative;
+      .overlay {
+        width: 100%;
+        height: 100%;
+        background: rgba(#000, 0.55);
+        opacity: 0;
+        position: absolute;
+        top: 0;
+        left: 0;
+        transition: 200ms;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        &:hover {
+          cursor: pointer;
+          opacity: 1;
+        }
+        .info {
+          color: #ddd;
           display: flex;
           flex-direction: row;
-          align-items: center;
-          justify-content: center;
-          &:hover {
-            cursor: pointer;
-            opacity: 1;
-          }
-          .info {
-            color: #ddd;
-            display: flex;
-            flex-direction: row;
-            margin: 0 10px;
-            p { margin: 0 0 0 5px; }
-          }
-          .likes {
-            p { margin: 0 0 0 8px; }
-          }
+          margin: 0 10px;
+          p { margin: 0 0 0 5px; }
         }
-        img {
-          width: 100%;
-          height: 100%;
+        .likes {
+          p { margin: 0 0 0 8px; }
         }
+      }
+      img {
+        width: 100%;
+        height: 100%;
       }
     }
   }
