453a2e5cdbffadd0a41a74169989a09c4f58f622
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-08-24 00:04:33 +0900

    コートのuiを作り替える

--- pages/court.vue
+++ pages/court.vue
@@ -5,22 +5,43 @@
       :background="background"
     />
     <main>
-      <div class="name">
-        <p>{{ court.name }}</p>
+      <div class="img-part">
+        <!-- ちゃんと配列になっているか？ -->
+        <img v-if="court.img[1] === undefined" src="@/assets/img/noimg.png" class="no-img">
+        <img v-if="court.img[1] !== undefined" :src="court.img[1]" class="court-img">
+      </div>
+      <div class="basic-info">
+        <h2 class="court-name">{{ court.name }}</h2>
+        <p class="pre-city">@{{ court.prefecture }}{{ court.city }}</p>
+        <div class="likes-bookmarks">
+          <label>
+            <HeartIcon />
+            <span>{{ court.likes }}</span>
+          </label>
+          <label>
+            <BookmarkIcon />
+            <span>{{ court.bookmarks }}</span>
+          </label>
+        </div>
       </div>
     </main>
-    <iframe :src="court.embedSrc" class="maps">
-    </iframe>
-    <!-- <p>{{ court }}</p> -->
+    <!-- <iframe :src="court.embedSrc" class="maps">
+    </iframe> -->
+    <!-- <p>{{ court.img[0] }}</p> -->
   </div>
 </template>
 
 <script>
 import firebase from '~/plugins/firebase';
+import HeartIcon from '@/assets/icons/HeartIcon';
+import BookmarkIcon from '@/assets/icons/BookmarkIcon';
 const db = firebase.firestore();
 
 export default {
-  created() {
+  components: {
+    HeartIcon, BookmarkIcon
+  },
+  mounted() {
     const id = this.$route.query.id;
     db.collection('court').doc(id).get()
       .then(court => {
@@ -49,7 +70,47 @@ export default {
 <style lang="scss" scoped>
 .court {
   main {
-    margin: 40px 0;
+    margin: 80px 0 0;
+    display: flex;
+    flex-wrap: wrap;
+    // justify-content: center;
+    .img-part {
+      background: #222;
+      width: 700px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      padding: 0 100px ;
+      .no-img {
+        width: 100%;
+        // height: auto;
+      }
+      .court-img {
+        width: 100%;
+      }
+    }
+    .basic-info {
+      background: #ddd;
+      padding-top: 30px;
+      margin: 0;
+      .court-name {
+        color: #262626;
+        margin: 0;
+      }
+      .pre-city {
+        margin: 0;
+      }
+      .likes-bookmarks {
+        display: flex;
+        flex-direction: row;
+        label {
+          display: flex;
+          flex-direction: row;
+          margin: 0 20px;
+        }
+      }
+    }
   }
   .maps {
     width: 100%;
