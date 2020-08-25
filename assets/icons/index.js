5731fa412ee709c3602d6b25f8a8227b8f9345b9
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-08-25 18:38:13 +0900

    iconをモジュール化

---
+++ assets/icons/index.js
@@ -0,0 +1,15 @@
+import BookmarkEmptyIcon from './BookmarkEmptyIcon';
+import BookmarkIcon from './BookmarkIcon';
+import GoogleIcon from './GoogleIcon';
+import HeartEmptyIcon from './HeartEmptyIcon';
+import HeartIcon from './HeartIcon';
+import SearchIcon from './SearchIcon';
+
+export {
+  BookmarkEmptyIcon,
+  BookmarkIcon,
+  GoogleIcon,
+  HeartEmptyIcon,
+  HeartIcon,
+  SearchIcon
+}
