f4fe44eff43d8d9c6cf24f2d1952b07e9ce65aec
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-08-25 19:33:39 +0900

    レイアウトがいい感じになってきた

--- pages/court.vue
+++ pages/court.vue
@@ -1,51 +1,82 @@
 <template>
   <div class="court">
-    <Header
-      :geoHoop="geoHoop"
-      :background="background"
-    />
+    <Header :geoHoop="geoHoop" :background="background" />
     <main>
-      <div class="img-part">
-        <!-- ちゃんと配列になっているか？ -->
-        <img v-if="court.img[1] === undefined" src="@/assets/img/noimg.png" class="no-img">
-        <img v-if="court.img[1] !== undefined" :src="court.img[1]" class="court-img">
-      </div>
-      <div class="basic-info">
-        <h2 class="court-name">{{ court.name }}</h2>
-        <p class="pre-city">@{{ court.prefecture }}{{ court.city }}</p>
-        <div class="likes-bookmarks">
-          <label>
-            <HeartIcon />
-            <span>{{ court.likes }}</span>
-          </label>
-          <label>
-            <BookmarkIcon />
-            <span>{{ court.bookmarks }}</span>
-          </label>
+      <div class="box col-md-6">
+        <div class="img-part">
+          <img
+            v-if="!Array.isArray(court.img)"
+            src="@/assets/img/noimg.png"
+            class="no-img"
+          >
+          <img v-else :src="court.img[0]" class="court-img">
+        </div>
+        <div class="box-footer">
+          <div class="upper-row">
+            <p class="pre-city">#{{ court.prefecture }} #{{ court.city }}</p>
+            <div class="likes-bookmarks">
+              <label @click="likesCount()">
+                <HeartEmptyIcon />
+                <span>{{ court.likes }}</span>
+              </label>
+              <label @click="bookmarksCount()">
+                <BookmarkEmptyIcon />
+                <span>{{ court.bookmarks }}</span>
+              </label>
+            </div>
+          </div>
+          <h2 class="court-name">{{ court.name }}</h2>
         </div>
       </div>
+      <div class="basic-info col-md-6">
+        <table>
+          <tr>
+            <td class="name">住所</td>
+            <td class="right">{{ court.address }}</td>
+          </tr>
+          <tr>
+            <td class="name">Google Maps</td>
+            <td class="right">
+              <a :href="court.googleMapsUrl" target="_blank">Google Mapsへ</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="name">参考URL</td>
+            <td class="right">
+              <a :href="court.refUrl" target="_blank">{{ court.refUrl }}</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="name">コートタイプ</td>
+            <td class="right">{{ isOutside }}</td>
+          </tr>
+          <!-- <tr>
+            <td class="name"></td>
+            <td class="right">{{ court }}</td>
+          </tr> -->
+        </table>
+      </div>
     </main>
-    <!-- <iframe :src="court.embedSrc" class="maps">
-    </iframe> -->
-    <!-- <p>{{ court.img[0] }}</p> -->
+    <iframe :src="court.embedSrc" class="maps"></iframe>
+    <FooterDefault />
   </div>
 </template>
 
 <script>
+import { HeartEmptyIcon, HeartIcon, BookmarkEmptyIcon, BookmarkIcon } from '@/assets/icons';
 import firebase from '~/plugins/firebase';
-import HeartIcon from '@/assets/icons/HeartIcon';
-import BookmarkIcon from '@/assets/icons/BookmarkIcon';
 const db = firebase.firestore();
 
 export default {
   components: {
-    HeartIcon, BookmarkIcon
+    HeartEmptyIcon, HeartIcon, BookmarkEmptyIcon, BookmarkIcon
   },
   mounted() {
     const id = this.$route.query.id;
     db.collection('court').doc(id).get()
       .then(court => {
         this.court = court.data();
+        this.isOutside = court.data().isOutside ? '屋外コート' : '屋内コート';
         console.log(this.court);
       })
       .catch(error => {
@@ -59,55 +90,98 @@ export default {
   },
   data() {0
     return {
-      court: '',
+      court: {},
+      isOutside: null,
       geoHoop: '#262626',
       background: '#fff'
     };
+  },
+  methods : {
+    likesCount() {
+      alert('いいねが押されました');
+    },
+    bookmarksCount() {
+      alert('ブックマークボタンが押されました');
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .court {
+  overflow-x: hidden;
   main {
-    margin: 80px 0 0;
+    margin: 40px 0 0;
     display: flex;
     flex-wrap: wrap;
-    // justify-content: center;
-    .img-part {
-      background: #222;
-      width: 700px;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-      padding: 0 100px ;
-      .no-img {
-        width: 100%;
-        // height: auto;
+    .box {
+      width: 795px;
+      padding: 0;
+      @media (min-width: 768px) { padding: 0 10px; }
+      .img-part {
+        // background: #222;
+        height: auto;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        .no-img {
+          width: 100%;
+        }
+        .court-img {
+          width: 100%;
+        }
       }
-      .court-img {
-        width: 100%;
+      .box-footer {
+        // background: #eee;
+        padding: 5px;
+        border-bottom: 1px solid #ddd;
+        .upper-row {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          .pre-city {
+            color: #19b5fe;
+            margin: 0;
+            font-size: 16px;
+            font-weight: 200;
+          }
+          .likes-bookmarks {
+            display: flex;
+            flex-direction: row;
+            label {
+              display: flex;
+              flex-direction: row;
+              margin: 0 0 0 5px;
+              &:hover { cursor: pointer; }
+              span { margin: 1px 5px; }
+            }
+          }
+        }
+        .court-name {
+          color: #262626;
+          margin: 0;
+          font-size: 30px;
+          font-weight: 100;
+        }
       }
     }
     .basic-info {
-      background: #ddd;
-      padding-top: 30px;
-      margin: 0;
-      .court-name {
-        color: #262626;
-        margin: 0;
-      }
-      .pre-city {
-        margin: 0;
-      }
-      .likes-bookmarks {
-        display: flex;
-        flex-direction: row;
-        label {
-          display: flex;
-          flex-direction: row;
-          margin: 0 20px;
+      // background: #ddd;
+      padding: 10px 5px;
+      table {
+        width: 100%;
+        table-layout: fixed;
+        tr {
+          height: 33px;
+          .name {
+            // background: red;
+            color: #606060;
+            width: 110px;
+          }
+          .right {
+            word-wrap: break-word;
+          }
         }
       }
     }
@@ -116,6 +190,7 @@ export default {
     width: 100%;
     height: 400px;
     border: none;
+    margin: 50px 0;
   }
 }
 </style>
